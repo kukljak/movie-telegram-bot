0064c9b0da0d99f4fdf0dd94c8245963be3ae306
kukljak <akukljak@gmail.com>
2022-06-17 23:51:30 +0300

    Add try-catch checking

--- botScenes/recordViewedFilm.js
+++ botScenes/recordViewedFilm.js
@@ -11,33 +11,40 @@ const recordViewedFilm = () => {
     let film;
     
     const movieNameHandler = Telegraf.on('text', async (ctx) => {
-        profile_id = ctx.scene.state.profile_id;
-        profile = await Profiles.findById(profile_id).populate('user');
+        try {
+            profile_id = ctx.scene.state.profile_id;
+            profile = await Profiles.findById(profile_id).populate('user');
 
-        film = {
-            message_id: ctx.message.message_id,
-            name: ctx.message.text,
-            timeAdd: ctx.message.date
-        }
+            film = {
+                message_id: ctx.message.message_id,
+                name: ctx.message.text,
+                timeAdd: ctx.message.date
+            }
         
-        await ctx.reply('Оцініть даний фільм від 1 до 10');
-        return ctx.wizard.next();
+            await ctx.reply('Оцініть даний фільм від 1 до 10');
+            return ctx.wizard.next();
+        } catch (error) {
+            console.error(error);
+        }
     });
 
     const movieVoteHandler = Telegraf.on('text', async (ctx) => {
-
-        if (/^([1-9]|10)$/.test(ctx.message.text)) {
-            film.vote = ctx.message.text;
-            profile.user.movies.push(film);
-            await profile.user.save();
-            ctx.reply('Фільм успішно додано у ваш список!');
+        try {
+            if (/^([1-9]|10)$/.test(ctx.message.text)) {
+                film.vote = ctx.message.text;
+                profile.user.movies.push(film);
+                await profile.user.save();
+                ctx.reply('Фільм успішно додано у ваш список!');
     
-            ctx.scene.leave();
-        } else {
-            await ctx.reply('Оцініть даний фільм цифрами від 1 до 10')
-            
+                ctx.scene.leave();
+            } else {
+                await ctx.reply('Оцініть даний фільм цифрами від 1 до 10');
+            }
+
+            await ctx.wizard.selectStep(1);
+        } catch (error) {
+            console.error(error);
         }
-        await ctx.wizard.selectStep(1);
     });
 
     const currentScene = new Scenes.WizardScene('recordViewedFilm', movieNameHandler, movieVoteHandler);
--- index.js
+++ index.js
@@ -22,50 +22,62 @@ bot.use(session());
 bot.use(stage.middleware());
 
 bot.start( async (ctx) => {
-    ctx.reply(`Привіт ${ctx.from.first_name}! Це твій персональний менеджер для фільмів. \nРазом з ним, ти можеш створити список переглянутих фільмів, оцінити його та вивести список в чат \n\nОбери одну з наступних дій`, Markup.inlineKeyboard([
-        Markup.button.callback('Переглянуті фільми','showMovies'),
-        Markup.button.callback('Додати переглянутий фільм', 'writeMovie')
-    ]));
-
-    userInfo = await Users.findOne({id: ctx.message.from.id});
-    chatInfo = await Chats.findOne({id: ctx.message.chat.id});
-
-    profileInfo = await Profiles.findOne({user: userInfo?._id});
-    profileExist = !!profileInfo;
+    try {
+        ctx.reply(`Привіт ${ctx.from.first_name}! Це твій персональний менеджер для фільмів. \nРазом з ним, ти можеш створити список переглянутих фільмів, оцінити його та вивести список в чат \n\nОбери одну з наступних дій`, Markup.inlineKeyboard([
+            Markup.button.callback('Переглянуті фільми','showMovies'),
+            Markup.button.callback('Додати переглянутий фільм', 'writeMovie')
+        ]));
     
-
-    if (profileExist) {
-        await Users.findOneAndUpdate({id: ctx.message.from.id}, {$set: ctx.message.from});
-        await Chats.findOneAndUpdate({id: ctx.message.chat.id}, {$set: ctx.message.chat});
-        profile_id = profileInfo._id;
-    } else {
-        const newUser = await Users.create(ctx.message.from);
-        const newChat = await Chats.create(ctx.message.chat);
-        profileInfo = await Profiles.create({user: newUser, chat: newChat});
-        profile_id = profileInfo._id;
+        userInfo = await Users.findOne({id: ctx.message.from.id});
+        chatInfo = await Chats.findOne({id: ctx.message.chat.id});
+    
+        profileInfo = await Profiles.findOne({user: userInfo?._id});
+        profileExist = !!profileInfo;
+        
+    
+        if (profileExist) {
+            await Users.findOneAndUpdate({id: ctx.message.from.id}, {$set: ctx.message.from});
+            await Chats.findOneAndUpdate({id: ctx.message.chat.id}, {$set: ctx.message.chat});
+            profile_id = profileInfo._id;
+        } else {
+            const newUser = await Users.create(ctx.message.from);
+            const newChat = await Chats.create(ctx.message.chat);
+            profileInfo = await Profiles.create({user: newUser, chat: newChat});
+            profile_id = profileInfo._id;
+        }
+        profileExist = false;
+    } catch (error) {
+        console.error(error);
     }
-    profileExist = false;
 })
 
 bot.action('writeMovie', async (ctx) => {
-    const profile = await getProfile(ctx);
-    profile_id = profile._id;
-    
-    await ctx.scene.enter('recordViewedFilm', {profile_id: profile_id});
+    try {
+        const profile = await getProfile(ctx);
+        profile_id = profile._id;
+        
+        await ctx.scene.enter('recordViewedFilm', {profile_id: profile_id});
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 bot.action('showMovies', async (ctx) => {
-    const profile = await getProfile(ctx);
-    profile_id = profile._id;
+    try {
+        const profile = await getProfile(ctx);
+        profile_id = profile._id;
+        
+        const { user } = await Profiles.findById(profile_id).populate('user');
+        let moviesList = '';
+        user.movies.map( (film,index) => {
+            let rating = getVoteToString(film.vote);
+            moviesList = moviesList.concat('\n',`${index+1}. `,film.name, '\n', rating);
+        });
     
-    const { user } = await Profiles.findById(profile_id).populate('user');
-    let moviesList = '';
-    user.movies.map( (film,index) => {
-        let rating = getVoteToString(film.vote);
-        moviesList = moviesList.concat('\n',`${index+1}. `,film.name, '\n', rating);
-    });
-
-    await ctx.reply(moviesList);
+        await ctx.reply(moviesList);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 
